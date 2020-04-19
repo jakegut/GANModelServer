28de302b27e0a8beefb25629590c5e516367a949
Jake Gutierrez <jakegut0108@gmail.com>
2020-04-19 14:07:34 -0500

    Support for multiple vectors

--- generate.py
+++ generate.py
@@ -11,7 +11,63 @@ import math
 import random
 from collections import OrderedDict
 
-def generate(modelToLoad:str, z_dims:int, nz:int) -> list:
+class Generator():
+
+    out_height = 11
+    out_width = 16
+    batch_size = 1
+    image_size = 32
+    ngf = 64
+    n_extra_layers = 0
+    ngpu = 1
+
+    def __init__(self, model_to_load, z_dims, nz):
+        self.nz = nz
+        self.z_dims = z_dims
+        self.generator = dcgan.DCGAN_G(self.image_size, nz, z_dims, self.ngf, self.ngpu, self.n_extra_layers)
+        deprecatedModel = torch.load(model_to_load, map_location=lambda storage, loc: storage)
+
+        fixedModel = OrderedDict()
+        for (goodKey,ignore) in self.generator.state_dict().items():
+            # Take the good key and replace the : with . in order to get the deprecated key so the associated value can be retrieved
+            badKey = goodKey.replace(":",".")
+            #print(goodKey)
+            #print(badKey)
+        # Some parameter settings of the generator.state_dict() are not actually part of the saved models
+        if badKey in deprecatedModel:
+            goodValue = deprecatedModel[badKey]
+            fixedModel[goodKey] = goodValue
+
+        if not fixedModel:
+            #print("LOAD REGULAR")
+            #print(deprecatedModel)
+            # If the fixedModel was empty, then the model was trained with the new labels, and the regular load process is fine
+            self.generator.load_state_dict(deprecatedModel)
+        else:
+            # Load the parameters with the fixed labels  
+            self.generator.load_state_dict(fixedModel)
+
+    def generate(self, vector=None):
+        random_latent = []
+        if vector:
+            random_latent = vector
+        else:
+            random_latent = [random.uniform(-1, 1) for _ in range(self.nz)]
+        latent_vector = torch.FloatTensor(random_latent).view(self.batch_size, self.nz, 1, 1) 
+        levels = self.generator(Variable(latent_vector, volatile=True))
+
+        #levels.data = levels.data[:,:,:14,:28] #Cut of rest to fit the 14x28 tile dimensions
+
+        level = levels.data.cpu().numpy()
+        level = level[:,:,:self.out_height,:self.out_width] #Cut of rest to fit the 14x28 tile dimensions
+        level = numpy.argmax( level, axis = 1)
+
+        # Jacob: Only output first level, since we are only really evaluating one at a time
+        return level[0].tolist()
+
+
+
+def generate(modelToLoad, z_dims, nz, vector=None):
     out_height = 11
     out_width = 16
     batchSize = 1
@@ -47,7 +103,11 @@ def generate(modelToLoad:str, z_dims:int, nz:int) -> list:
         # Load the parameters with the fixed labels  
         generator.load_state_dict(fixedModel)
 
-    random_latent = [ random.uniform(-1.0, 1.0) ]*nz
+    random_latent = []
+    if vector:
+        random_latent = vector
+    else:
+        random_latent = [ random.uniform(-1.0, 1.0) ]*nz
     latent_vector = torch.FloatTensor(random_latent).view(batchSize, nz, 1, 1) 
     levels = generator(Variable(latent_vector, volatile=True))
 
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
-from generate import generate
+from generate import Generator
 import json
 
 app = Flask(__name__)
 
-@app.route("/api/get-level")
+gen = Generator("ZeldaGAN/ZeldaFixedDungeonsAlNoDoors_10000_10.pth", 6, 10)
+
+@app.route("/api/get-level", methods=['GET'])
 def get_level():
-    level = generate("ZeldaGAN/ZeldaFixedDungeonsAlNoDoors_10000_10.pth", 6, 10)
+    level = gen.generate()
+    return jsonify(level)
+
+@app.route("/api/get-level", methods=['POST'])
+def get_level_post():
+    data = request.get_json()
+
+    level = [gen.generate(vector=v) for v in data['vectors']]
     return jsonify(level)
 
 if __name__ == "__main__":
